d5695e7fcd3152fa7b25ede6a45a3ded3038b18d
KissLeventeMagor <117647198+KissLeventeMagor@users.noreply.github.com>
2024-12-01 22:35:38 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace gyakhalo
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<Mountain> Mountains { get; set; } = new ObservableCollection<Mountain>();
+      
+        private Mountain _selecteditem;
+        public Mountain SelectedItem
+        {
+            get => _selecteditem;
+            set
+            {
+                if(_selecteditem != value)
+                {
+                    _selecteditem = value;
+                    OnPropertyChanged(nameof(SelectedItem));
+                }
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            Mountains.Add(new Mountain("János-hegy",527));
+            Mountains.Add(new Mountain("Kis-Hárs-hegy",362));
+            Mountains.Add(new Mountain("Nagy-Hárs-hegy",454));
+            Mountains.Add(new Mountain("Hármashatár-hegy",495));
+        }
+
+        public void ButtonClicked(object sender, EventArgs e)
+        {
+            foreach(Mountain a in Mountains)
+            {
+                a.Climbed = false;
+            }
+        }
+    }
+
+}
---
+++ Mountain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyakhalo
+{
+    public class Mountain : INotifyPropertyChanged
+    {
+        private string _name;
+        private int _height;
+        private bool _climbed;
+        private Color _textcolor;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if(_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                if(_height != value)
+                {
+                    _height = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public bool Climbed
+        {
+            get => _climbed;
+            set
+            {
+                if( _climbed != value)
+                {
+                    _climbed = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Color TextColor
+        {
+            get => _textcolor;
+            set
+            {
+                if (_textcolor != value)
+                {
+                    _textcolor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Mountain(string name, int height)
+        {
+            Name = name;
+            Height = height;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
